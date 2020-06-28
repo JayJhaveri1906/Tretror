a522b6049f8dc2b18daf5e93c5bfc6fb453e4322
Sanjul <sanjul@users.noreply.github.com>
2020-06-28 21:30:23 +0530

    Some cleanup:

--- lib/components/pages/ProfilePage.dart
+++ lib/components/pages/ProfilePage.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter95/flutter95.dart';
-import 'package:tretror/components/pages/CrawwItPage.dart';
 import 'package:tretror/components/ui/Posts.dart';
 //import 'package:tretror/components/ui/RetroButton.dart';
 import 'package:tretror/components/ui/RetroTab.dart';
@@ -21,12 +20,9 @@ class ProfilePage extends StatelessWidget {
             Container(
                 padding: EdgeInsets.only(left: 35.0),
                 transform: Matrix4.translationValues(0.0, -20.0, 0.0),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(3.0),
-                  child: Image.network(
-                    'https://hemmy.xyz/tretror/profile_photo.png',
-                    width: 50.0,
-                    fit: BoxFit.fill,
+                child: CircleAvatar(
+                  backgroundImage: NetworkImage(
+                    "https://pbs.twimg.com/profile_images/864282616597405701/M-FEJMZ0_400x400.jpg",
                   ),
                 )),
             Padding(
@@ -40,28 +36,28 @@ class ProfilePage extends StatelessWidget {
                       padding: EdgeInsets.only(left: 10.0),
                       child: Text('Google Inc.',
                           style: TextStyle(
-                              fontSize: 16, color: Color(0xff767676))))
+                              fontSize: 12, color: Color(0xff767676))))
                 ])),
             Padding(
                 padding: EdgeInsets.only(left: 35.0, top: 15.0, bottom: 35.0),
                 child: Row(children: <Widget>[
                   Row(children: <Widget>[
-                    Text('300', style: TextStyle(fontSize: 16)),
+                    Text('300', style: TextStyle(fontSize: 12)),
                     Padding(
                         padding: EdgeInsets.only(left: 10.0),
                         child: Text('Followers',
                             style: TextStyle(
-                                fontSize: 16, color: Color(0xff767676))))
+                                fontSize: 12, color: Color(0xff767676))))
                   ]),
                   Row(children: <Widget>[
                     Padding(
                         padding: EdgeInsets.only(left: 25.0),
-                        child: Text('300', style: TextStyle(fontSize: 16))),
+                        child: Text('300', style: TextStyle(fontSize: 12))),
                     Padding(
                         padding: EdgeInsets.only(left: 10.0),
                         child: Text('Following',
                             style: TextStyle(
-                                fontSize: 16, color: Color(0xff767676))))
+                                fontSize: 12, color: Color(0xff767676))))
                   ])
                 ])),
             // Row(
