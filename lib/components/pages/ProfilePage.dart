0d5c447f4313d237e9e30d7fde459d3260525a47
Sanjul <sanjul@users.noreply.github.com>
2020-06-28 20:31:13 +0530

    Changed some default fonts

--- lib/components/pages/ProfilePage.dart
+++ lib/components/pages/ProfilePage.dart
@@ -11,73 +11,76 @@ class ProfilePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold95(
       title: "Profile",
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Image.network('https://hemmy.xyz/tretror/banner.png',
-              width: MediaQuery.of(context).size.width, fit: BoxFit.fill),
-          Container(
-              padding: EdgeInsets.only(left: 35.0),
-              transform: Matrix4.translationValues(0.0, -20.0, 0.0),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(3.0),
-                child: Image.network(
-                  'https://hemmy.xyz/tretror/profile_photo.png',
-                  width: 50.0,
-                  fit: BoxFit.fill,
-                ),
-              )),
-          Padding(
-              padding: EdgeInsets.only(left: 35.0),
-              child: Text('Sundar Pichai', style: TextStyle(fontSize: 16))),
-          Padding(
-              padding: EdgeInsets.only(left: 35.0, top: 15.0),
-              child: Row(children: <Widget>[
-                Image.network('https://hemmy.xyz/tretror/location.png'),
-                Padding(
-                    padding: EdgeInsets.only(left: 10.0),
-                    child: Text('Google Inc.',
-                        style:
-                            TextStyle(fontSize: 16, color: Color(0xff767676))))
-              ])),
-          Padding(
-              padding: EdgeInsets.only(left: 35.0, top: 15.0, bottom: 35.0),
-              child: Row(children: <Widget>[
-                Row(children: <Widget>[
-                  Text('300', style: TextStyle(fontSize: 16)),
+      body: DefaultTextStyle(
+        style: Theme.of(context).textTheme.headline6,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Image.network('https://hemmy.xyz/tretror/banner.png',
+                width: MediaQuery.of(context).size.width, fit: BoxFit.fill),
+            Container(
+                padding: EdgeInsets.only(left: 35.0),
+                transform: Matrix4.translationValues(0.0, -20.0, 0.0),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(3.0),
+                  child: Image.network(
+                    'https://hemmy.xyz/tretror/profile_photo.png',
+                    width: 50.0,
+                    fit: BoxFit.fill,
+                  ),
+                )),
+            Padding(
+                padding: EdgeInsets.only(left: 35.0),
+                child: Text('Sundar Pichai', style: TextStyle(fontSize: 16))),
+            Padding(
+                padding: EdgeInsets.only(left: 35.0, top: 15.0),
+                child: Row(children: <Widget>[
+                  Image.network('https://hemmy.xyz/tretror/location.png'),
                   Padding(
                       padding: EdgeInsets.only(left: 10.0),
-                      child: Text('Followers',
+                      child: Text('Google Inc.',
                           style: TextStyle(
                               fontSize: 16, color: Color(0xff767676))))
-                ]),
-                Row(children: <Widget>[
-                  Padding(
-                      padding: EdgeInsets.only(left: 25.0),
-                      child: Text('300', style: TextStyle(fontSize: 16))),
-                  Padding(
-                      padding: EdgeInsets.only(left: 10.0),
-                      child: Text('Following',
-                          style: TextStyle(
-                              fontSize: 16, color: Color(0xff767676))))
-                ])
-              ])),
-          // Row(
-          //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          //     children: <Widget>[
-          //       RetroButton(onPressed: null, text: 'Krawws'),
-          //       RetroButton(onPressed: null, text: 'Reply Kraws'),
-          //       RetroButton(onPressed: null, text: 'Media'),
-          //       RetroButton(onPressed: null, text: 'Likes')
-          //     ])
+                ])),
+            Padding(
+                padding: EdgeInsets.only(left: 35.0, top: 15.0, bottom: 35.0),
+                child: Row(children: <Widget>[
+                  Row(children: <Widget>[
+                    Text('300', style: TextStyle(fontSize: 16)),
+                    Padding(
+                        padding: EdgeInsets.only(left: 10.0),
+                        child: Text('Followers',
+                            style: TextStyle(
+                                fontSize: 16, color: Color(0xff767676))))
+                  ]),
+                  Row(children: <Widget>[
+                    Padding(
+                        padding: EdgeInsets.only(left: 25.0),
+                        child: Text('300', style: TextStyle(fontSize: 16))),
+                    Padding(
+                        padding: EdgeInsets.only(left: 10.0),
+                        child: Text('Following',
+                            style: TextStyle(
+                                fontSize: 16, color: Color(0xff767676))))
+                  ])
+                ])),
+            // Row(
+            //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            //     children: <Widget>[
+            //       RetroButton(onPressed: null, text: 'Krawws'),
+            //       RetroButton(onPressed: null, text: 'Reply Kraws'),
+            //       RetroButton(onPressed: null, text: 'Media'),
+            //       RetroButton(onPressed: null, text: 'Likes')
+            //     ])
 
-          RetroTabView(tabs: [
-            RetroTab(tabHeader: "Krawws", body: Posts()),
-            RetroTab(tabHeader: "Reply Krawws", body: Icon(Icons.message)),
-            RetroTab(tabHeader: "Media", body: Icon(Icons.video_call)),
-            RetroTab(tabHeader: "Likes", body: Icon(Icons.thumb_up)),
-          ])
-        ],
+            RetroTabView(tabs: [
+              RetroTab(tabHeader: "Krawws", body: Posts()),
+              RetroTab(tabHeader: "Reply Krawws", body: Icon(Icons.message)),
+              RetroTab(tabHeader: "Media", body: Icon(Icons.video_call)),
+              RetroTab(tabHeader: "Likes", body: Icon(Icons.thumb_up)),
+            ])
+          ],
+        ),
       ),
     );
   }
