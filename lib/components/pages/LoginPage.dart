51f7b66a01e39459d8d573364107e2f831c871b3
Hemmy <hemnathmouli@gmail.com>
2020-06-28 13:07:46 +0530

    Fix error on login page

--- lib/components/pages/LoginPage.dart
+++ lib/components/pages/LoginPage.dart
@@ -5,37 +5,48 @@ class LoginPage extends StatelessWidget {
 	@override
 	Widget build ( BuildContext context ) {
 		return Scaffold95(
-			title: 'LOGIN'
-			body: Center(
+        body: Center(
             child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-          Image.network('https://hemmy.xyz/tretror/logo.png',
-              height: 60, fit: BoxFit.fill),
-          Container(
-              padding: EdgeInsets.only(top: 40.0),
-              width: 300.0,
-              child: Column(children: <Widget>[
-                TextField95(
-                    decoration: InputDecoration(
-                      labelText: 'Username',
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image.network('https://hemmy.xyz/tretror/logo.png',
+                  height: 60, fit: BoxFit.fill),
+              Container(
+                  padding: EdgeInsets.only(top: 40.0),
+                  width: 300.0,
+                  child: Column(children: <Widget>[
+                    Elevation95(
+                      type: Elevation95Type.down,
+                      child: Container(
+                        color: Colors.white,
+                        child: TextField(
+                            decoration: InputDecoration(
+                              labelText: 'Username',
+                            ),
+                            style: TextStyle(color: Colors.black54)),
+                      ),
                     ),
-                    style: TextStyle(color: Colors.black54)),
-                TextField95(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      labelText: 'Password',
+                    Elevation95(
+                      type: Elevation95Type.down,
+                      child: Container(
+                        color: Colors.white,
+                        child: TextField(
+                            obscureText: true,
+                            decoration: InputDecoration(
+                              labelText: 'Password',
+                            ),
+                            style: TextStyle(color: Colors.black54)),
+                      ),
                     ),
-                    style: TextStyle(color: Colors.black54)),
-                Padding(
-                    padding: EdgeInsets.only(top: 40.0),
-                    child:
-                        Button95(onPressed: () {}, child: Text('Login'))),
-                FlatButton(
-                    onPressed: () {},
-                    child: Text('Don\'t have an account? Create account.'))
-              ]))
-        ]))
-		);
+                    Padding(
+                        padding: EdgeInsets.only(top: 40.0),
+                        child: Button95(onTap: () {}, child: Text('Login'))),
+                    FlatButton(
+                        onPressed: () {},
+                        child: Text('Don\'t have an account? Create account.'))
+                  ]))
+            ]),
+        ),
+        );
 	}
 }
